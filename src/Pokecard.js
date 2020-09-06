ee319d59735d2be0b90363b9fd753265f1022929
Beau Sherman <brsherman88@gmail.com>
2020-09-05 20:06:05 -0700

    Add formatId function and change image src

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -3,11 +3,17 @@ import './Pokecard.css';
 
 class Pokecard extends Component {
   render() {
+    function formatId(id) {
+      const num = 3 - String(id).length;
+      return "0".repeat(num) + id;
+    }
+
     const { name, id, type, exp } = this.props;
+
     return (
       <div className="Pokecard">
         <h2>{name}</h2>
-        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} alt={name}></img>
+        <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${formatId(id)}.png`} alt={name}></img>
         <p>Type: {type}</p>
         <p>EXP: {exp}</p>
       </div>
